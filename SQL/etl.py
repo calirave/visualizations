e06e6c4a14f212a8d50b541089f1b535b5053edd
calirave <49568918+calirave@users.noreply.github.com>
2023-10-05 21:32:08 -0400

    Update etl.py

--- SQL/etl.py
+++ SQL/etl.py
@@ -11,30 +11,7 @@ cursor = conn.cursor()
 # cursor.execute("SELECT * FROM geography")
 cursor.execute("SELECT * FROM filtered_vac")
 rows = cursor.fetchall()
-# column_name = [desc[0] for desc in cursor.description]
-# conn.close
 
-# # convert the rows into list of dictionaries
-# mydata = []
-# for row in rows:
-#     data_dict = {}
-#     for i in range(len(column_name)):
-#         data_dict[column_name[i]] = row[i]
-#     mydata.append(data_dict)
-
-# # # convert the list into JSON object
-# myjson = json.dumps(mydata)
-
-# # # export file path
-# # # jsonexportpath = "demographics.json"
-# # #jsonexportpath = "geography.json"
-# jsonexportpath = "vaccinations.json"
-
-# # # write to file
-# with open(jsonexportpath, "w") as json_file:
-#     json.dump(myjson, json_file, indent=4)
-
-########################
 # new code to try dumping to a json array
 result = []
 for row in rows:
